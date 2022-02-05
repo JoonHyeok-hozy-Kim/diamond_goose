bb92562a5c4cf6ff2a2004dd32922855b419a783
hozy <danielisdan@naver.com>
2022-02-05 16:44:08 +0900

    currencymaster country field null_true temporarily

--- masterinfoapp/models.py
+++ masterinfoapp/models.py
@@ -9,7 +9,7 @@ class CurrencyMaster(models.Model):
     currency_name = models.CharField(max_length=100, null=False)
     currency_sign = models.CharField(max_length=10, null=True)
     currency_national_flag = models.ImageField(upload_to='currencymaster/', null=True)
-    country = models.CharField(max_length=100, null=False)
+    country = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         result_list = [self.currency_name]
